d7d52818471a148cdb5c5e9c9f2c209b99526f25
avveshka <aleksandr_androsov@mail.ru>
2019-03-13 23:24:53 +0300

    Update Program.cs

--- Androsov_Alexander_Task04/Task07/Program.cs
+++ Androsov_Alexander_Task04/Task07/Program.cs
@@ -13,7 +13,7 @@ namespace Task07
         {
             Console.WriteLine("Введите текст со временем : ");
             string input = Console.ReadLine();
-            string pattern = @"(([0-23])|(0[0-9])):(([0-5][0-9])|(60))";
+            string pattern = @"(([0-1][0-9])|(2[0-3])|[0-7]):(([0-5][0-9])|(60))";
             Regex regex = new Regex(pattern);
             MatchCollection matches = regex.Matches(input);
             Console.WriteLine($"Во введённой строке {matches.Count} раз встречается время");
